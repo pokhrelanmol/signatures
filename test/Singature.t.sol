51f1a4dfac4ef32dea17d16326abcbc700a27285
anmol <anmolpokhrel46@gmail.com>
2024-11-06 06:32:36 +0530

    added simple replay attack test

--- test/Singature.t.sol
+++ test/Singature.t.sol
@@ -46,4 +46,37 @@ contract SignatureTest is Test {
         signature.setNumberWithSignatureEIP191(number, _signature);
         assertEq(signature.number(), number);
     }
+
+    //The implentation is not protected against replay attack
+
+    function testSignatureReplayAttack() public {
+        uint256 number = 10;
+
+        bytes1 prefix = bytes1(0x19);
+        bytes1 eip191Version = bytes1(0);
+        address indendedValidatorAddress = address(signature);
+        bytes32 applicationSpecificData = keccak256(abi.encodePacked(number));
+        bytes32 messageHash = keccak256(
+            abi.encodePacked(
+                prefix,
+                eip191Version,
+                indendedValidatorAddress,
+                applicationSpecificData
+            )
+        );
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(key, messageHash);
+        bytes memory _signature = abi.encodePacked(r, s, v);
+        //increment
+        signature.setNumberWithSignatureEIP191(number, _signature);
+        assertEq(signature.number(), number);
+
+        //@note The same signature can be used again to set the number, Imagine this
+        // was a token transfer rather then setting the number, this would have drained all the
+        // user funds.
+
+        signature.setNumberWithSignatureEIP191(number, _signature);
+
+        assertEq(signature.number(), number);
+    }
 }
